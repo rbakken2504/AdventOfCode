d0dcffa44f88f64c0ba0d967f84c1e41b6499a82
Ryan Bakken <ryan.bakken@atlas-x.com>
2020-12-14 17:51:07 -0600

    Brute Force #2

--- AdventOfCode/Day13/Part2.cs
+++ AdventOfCode/Day13/Part2.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AdventOfCode.Day13
 {
@@ -9,27 +11,38 @@ namespace AdventOfCode.Day13
     {
         public static void Solve()
         {
-            Dictionary<int, int> busOffsets = ParseBusOffsets();
+            SortedDictionary<int, int> busOffsets = ParseBusOffsets();
             Dictionary<int, long> busTimestampFound = busOffsets.ToDictionary(kvp => kvp.Key, _ => -1L);
 
             int multiple = busOffsets.First().Key;
-            long timestamp = busOffsets.First().Key;
+            int offset = busOffsets.First().Value;
+            long timestamp = multiple;
+
+            foreach (KeyValuePair<int,int> keyValuePair in busOffsets)
+            {
+                Console.WriteLine($"{keyValuePair.Key} => {keyValuePair.Value}");
+            }
 
             while (BusTimestampNotFound(busTimestampFound))
             {
                 busTimestampFound = ResetBusTimestamps(busTimestampFound);
                 foreach (KeyValuePair<int,int> busOffset in busOffsets)
                 {
-                    if ((timestamp + busOffset.Value) % busOffset.Key == 0)
+                    if ((timestamp - offset + busOffset.Value) % busOffset.Key == 0)
                     {
-                        busTimestampFound[busOffset.Key] = timestamp;
+                        busTimestampFound[busOffset.Key] = timestamp - offset + busOffset.Value;
+                    }
+                    else
+                    {
+                        break;
                     }
                 }
 
                 timestamp += multiple;
+                Console.WriteLine($"Timestamp to Try: {timestamp}");
             }
 
-            Console.WriteLine($"Earliest Timestamp: {busTimestampFound[multiple]}");
+            Console.WriteLine($"Earliest Timestamp: {busTimestampFound[busOffsets.First(kvp => kvp.Value == 0).Key]}");
         }
 
         private static Dictionary<int, long> ResetBusTimestamps(Dictionary<int, long> busTimestamps)
@@ -42,11 +55,11 @@ namespace AdventOfCode.Day13
             return busTimestamps.Any(busTimestamp => busTimestamp.Value == -1);
         }
 
-        private static Dictionary<int, int> ParseBusOffsets()
+        private static SortedDictionary<int, int> ParseBusOffsets()
         {
             var file = new StreamReader(@"/Users/rbakken/RiderProjects/AdventOfCode/AdventOfCode/Day13/day_13.txt");
             string line;
-            var result = new Dictionary<int, int>();
+            var result = new SortedDictionary<int, int>(Comparer<int>.Create((x, y) => y.CompareTo(x)));
 
             while ((line = file.ReadLine()) != null)
             {
